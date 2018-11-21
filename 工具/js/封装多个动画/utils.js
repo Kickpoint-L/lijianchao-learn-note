e3075ed9af4a9be25a4b8504355fd45dca6f02d6
Kickpoint-L <775430971@qq.com>
2018-11-21 17:10:37 +0800

    learn note

--- 工具/js/封装多个动画/utils.js
+++ 工具/js/封装多个动画/utils.js
@@ -4,19 +4,41 @@ function  animate(el,params,fn) {
 		var flag = true;
 		//遍历params对象
 		for(var key in params){
+			//获取目标值
+			var target = params[key];
+			if(key=='zIndex'){
+				//因为zIndex 不存在过渡过程 所以直接给结果
+				el.style.zIndex = target;
+				//本次循环结束后面的代码不再执行,执行下一次循环
+				continue;
+			}
+
 			// offsetxx都是整数
 			var leader = getStyle(el,key);
+			if(key == 'opacity'){
+				//把透明度扩大100倍
+				leader = Math.floor(parseFloat(leader)*100);
+				//修改目标值
+				target = target*100;
+			}
 
-			var step = (params[key]-leader)/10;
+
+			var step = (target-leader)/10;
 			//修正step
 			if(Math.abs(step)<1){
 				step = step>0?Math.ceil(step):Math.floor(step);
 			}
-
-			//计算当前位置
-			el.style[key] = (leader+step)+'px';
+			if(key == 'opacity'){
+				//还原opacity的值
+				el.style.opacity = (leader+step)/100;
+			}else{
+				//计算当前位置
+				el.style[key] = (leader+step)+'px';
+			}
+					
 			//相等的时候清除
-			if(leader != params[key]){
+			//所有要执行动画的值与目标值相等时返回true.
+			if(leader != target){
 				flag = false;
 			}
 		}
@@ -28,22 +50,20 @@ function  animate(el,params,fn) {
 				fn();
 			}
 		}
-
 	},17)
 
 }
 
-/**
-* 根据class属性获取值
-**/
+		/**
+		* 根据class属性获取值
+		**/
 function getStyle (el,attr) {
 	//获取元素el的attr的class属性
 	var rs = window.getComputedStyle(el,null)[attr];
-	//把px去掉 返回number   20px
+	//把px去掉 返回number  
 	if(rs.indexOf('px')!=-1){
 		//字符串截取
 		rs = rs.substr(0,rs.length-2);
-		//变为number
 		rs = parseInt(rs);
 	}
 	return rs;
